09640ccec19555a2b5c89c4d7dc57798b7343451
Bob Corsaro <bob@embed.ly>
2014-01-23 16:27:51 +0100

    Update favicon

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'handlebars');
 app.set('host', process.env.HOST || 'http://localhost:3000');
-app.use(express.favicon());
+app.use(express.favicon(__dirname + '/public/images/favicon.png'));
 app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
@@ -37,6 +37,7 @@ app.get('/share/:id', routes.share);
 app.get('/image/(:id).png', routes.image);
 app.get('/view/:id', routes.view);
 app.get('/about', routes.about);
+app.get('/reset', routes.reset);
 
 http.createServer(app).listen(app.get('port'), "0.0.0.0", function(){
   console.log('Express server listening on port ' + app.get('port'));
